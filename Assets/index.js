ce6dd3b8427a5e5210e1900a2ef497249f633299
Jaryot <jarram999@gmail.com>
2023-04-29 14:27:27 -0700

    formatted files to be cleaner

--- Assets/index.js
+++ Assets/index.js
@@ -1,63 +1,61 @@
-
-const inquirer = require ('inquirer');
-const fs = require ('fs');
-const Shapes = require ('./lib/shapes');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Shapes = require("./lib/shapes");
 const Circle = Shapes.Circle;
 const Triangle = Shapes.Triangle;
 const Rectangle = Shapes.Rectangle;
 
-
-inquirer.prompt ([
+inquirer
+  .prompt([
     {
-        name: 'text',
-        message: 'Enter 3 letters to be on your logo',
-        type: 'input'
-    }, 
-    {
-        name: 'textColor',
-        message: 'What color would you like the logo TEXT to be?',
-        type: 'input'
-    }, 
-    {
-        name: 'shape',
-        message: 'Whhich shape would you like your logo to be?',
-        type: 'list',
-        choices: ['Circle', 'Triangle', 'Rectangle']
+      name: "text",
+      message: "Enter 3 letters to be on your logo",
+      type: "input",
     },
     {
-        name: 'shapeColor',
-        message: 'What color would you like the SHAPE of the logo to be?',
-        type: 'input'
+      name: "textColor",
+      message: "What color would you like the logo TEXT to be?",
+      type: "input",
     },
-   
+    {
+      name: "shape",
+      message: "Whhich shape would you like your logo to be?",
+      type: "list",
+      choices: ["Circle", "Triangle", "Rectangle"],
+    },
+    {
+      name: "shapeColor",
+      message: "What color would you like the SHAPE of the logo to be?",
+      type: "input",
+    },
+  ])
+  .then((answers) => {
+    fs.writeFile("logo.svg", fileGenerator(answers), (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("completed");
+    });
+  });
 
-]).then (answers => {
-    fs.writeFile ('logo.svg', fileGenerator(answers), (err)=>{
-        if (err){
-            return console.log(err);
-        }
-        console.log ('completed');
-    })
-});
+const fileGenerator = ({ text, textColor, shape, shapeColor }) => {
+  let newShape;
+  switch (shape) {
+    case "Circle":
+      newShape = new Circle(text, textColor, shapeColor);
+      break;
 
-const fileGenerator = ({text, textColor, shape, shapeColor}) => {
-    let newShape;
-    switch (shape) {
-        case 'Circle': 
-        newShape = new Circle (text, textColor, shapeColor);
-        break;
+    case "Triangle":
+      newShape = new Triangle(text, textColor, shapeColor);
+      break;
 
-        case 'Triangle':
-        newShape = new Triangle (text, textColor, shapeColor);
-        break;
+    case "Rectangle":
+      newShape = new Rectangle(text, textColor, shapeColor);
+      break;
+  }
 
-        case 'Rectangle':
-        newShape = new Rectangle (text, textColor, shapeColor);
-        break;
-    }
-    
-    return `${newShape.render()}`;
-}
+  return `${newShape.render()}`;
+};
 
 function indexstart() {}
 
--- Assets/lib/shapes.test.js
+++ Assets/lib/shapes.test.js
@@ -1,30 +1,33 @@
-const Shapes = require ('./shapes');
+const Shapes = require("./shapes");
 const Circle = Shapes.Circle;
 const Triangle = Shapes.Triangle;
 const Rectangle = Shapes.Rectangle;
 
-describe('Shapes', () => {
-    describe('Add', () => {
-        it('should take 3 parameters and display them properly on the triangle shape', () => {
-            const shape = new Triangle("SVG", 'red', 'darkblue');
-            expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+describe("Shapes", () => {
+  describe("Add", () => {
+    it("should take 3 parameters and display them properly on the triangle shape", () => {
+      const shape = new Triangle("SVG", "red", "darkblue");
+      expect(shape.render())
+        .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 <polygon points="150,0 25,198 275,198" fill="darkblue" stroke="black" stroke-width="2"/>
 <text x="150" y="140" font-family="Georgia Italic" font-size="60" text-anchor="middle" fill="red">SVG</text>
 </svg>`);
-        });
-        it('should take 3 parameters and display them properly on the circle shape', () => {
-            const shape = new Circle("EG", 'lightblue', 'black');
-            expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    });
+    it("should take 3 parameters and display them properly on the circle shape", () => {
+      const shape = new Circle("EG", "lightblue", "black");
+      expect(shape.render())
+        .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 <circle cx="150" cy="100" r="98" fill="black" stroke="black" stroke-width="2"/>
 <text x="150" y="122" font-family="Georgia Italic" font-size="60" text-anchor="middle" fill="lightblue">EG</text>
 </svg>`);
-        });
-        it('should take 3 parameters and display them properly on the reactangle shape', () => {
-            const shape = new Rectangle("IT", '#000', 'lightgreen');
-            expect(shape.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+    });
+    it("should take 3 parameters and display them properly on the reactangle shape", () => {
+      const shape = new Rectangle("IT", "#000", "lightgreen");
+      expect(shape.render())
+        .toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 <rect x="50" y="25" width="200" height="150" fill="lightgreen" stroke="black" stroke-width="2"/>
 <text x="150" y="125" font-family="Georgia Italic" font-size="60" text-anchor="middle" fill="#000">IT</text>
 </svg>`);
-        });
-    })
+    });
+  });
 });
