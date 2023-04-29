9e66451c862700c6c1f67ac4bd309f6039b30a8e
Jaryot <jarram999@gmail.com>
2023-04-29 13:35:21 -0700

    set-up first class and test.js file

--- index.js
+++ index.js
@@ -1,138 +1,42 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const jest = require("jest");
 
-const readMeDoc = ({
-  projectTitle,
-  motivation,
-  purpose,
-  solve,
-  learn,
-  installation,
-  usage,
-  credits,
-  license,
-  gitname,
-  email,
-}) => {
-
-  return `
-
-    # ${projectTitle} 
-
-    ## Description
-
-    ${motivation}
-
-    ${purpose}
-
-    ${solve}
-
-    ${learn}
-
-    ## Table of Contents
-    
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-
-    ## Installation 
-
-    ${installation}
-
-    ## Usage 
-
-    ${usage} 
-
-    ## Credit
-
-    ${credits} 
-
-    ## License
-    
-    This project uses the following license: ${license}. Please refer to [https://choosealicense.com/](https://choosealicense.com/).
-
-    ## Questions
-
-    You can see my GitHub Profile at https://github.com/${gitname}.
-
-    You can reach me at my email: ${email}
-
-    All code source should be found in the index.js file within the repo.
-    `;
-};
+const svgAttributes = ({
+    text,
+    textColor,
+    shape,
+    shapeColor,
+  }) => {}
 
 inquirer
   .prompt([
     {
       type: "input",
-      name: "projectTitle",
-      message: "What is your project title? ",
+      name: "text",
+      message: "Please input up to 3 characters for the logo.",
     },
     {
         type: "input",
-      message: "What was your motivation for this project?",
-      name: "motivation",
-    },
-    {
-        type: "input",
-      message: "Why did you build this project?",
-      name: "purpose",
-      
-    },
-    {
-        type: "input",
-      message: "What problem does this project solve?",
-      name: "solve",
-      
-    },
-    {
-        type: "input",
-      message: "What did you learn while creating this project?",
-      name: "learn",
-      
-    },
-    {
-        type: "input",
-      message: "What steps are required to install your project? Provide a step by step description of how to get the development enviornment running.",
-      name: "installation",
-
-    },
-    {
-        type: "input",
-      message: "Please provide instructions and examples for project use?",
-      name: "usage",
-      
-    },
-    {
-        type: "input",
-      message: "Please list any colaborators of this project or type in `none`",
-      name: "credits",
-      
+      message: "What color would you like the logo text to be?",
+      name: "textColor",
     },
     {
         type: "list",
-      message: "Which open source license is needed for your project?",
-      name: "license",
-      choices: ["Unlicense", "MIT", "GP", "Apache", "None"],
+      message: "What shape would you like the logo to be?",
+      name: "shape",
+      choices: ["Circle", "Triangle", "Rectangle"],
       
     },
     {
         type: "input",
-      message: "What is your Github user name?",
-      name: "gitname",
-      
-    },
-    {
-      type: "input",
-      message: "What is your email?",
-      name: "email",
-    
+      message: "What color would you like the logo to be?",
+      name: "shapeColor",
     },
   ])
   .then((answers) => {
     console.log(answers)
-    const filename = `${answers.projectTitle.toLowerCase()}.md`
+    const filename = `logo.svg`
 
     fs.writeFile(filename, readMeDoc(answers), (err) =>
       err ? console.log(err) : console.log("Success!")
--- readme.md
+++ readme.md
@@ -4,14 +4,6 @@
 
     ## Description
 
-    To create a node.js app that could create a high quality README file based on some prompts from Inquirer
-
-    I built this project as an exercise to the assignment for Module 9
-
-    It solves the issue of manually inputting data into a README file
-
-    I learned to use node and different modules in order to create, store, and populate data
-
     ## Table of Contents
     
     - [Installation](#installation)
@@ -21,7 +13,7 @@
 
     ## Installation 
 
-    To create the project, I installed the node.js package from their main website. After that, in my CLI, i downloaded the the inquirer and fs packages using npm i inquirer@8.2.4 in the CLI. 
+    
 
     ## Credit
 
@@ -34,10 +26,6 @@
     ## Questions
 
     You can see my GitHub Profile at https://github.com/zelada5574.
-
-    You can reach me at my email: jarram999@gmail.com
-
-    All code source should be found in the index.js file within the repo.
 
     https://github.com/zelada5574/Are-U-Reading
     https://drive.google.com/file/d/10CQEPWKzS7MJIF2TgBeZi-yHTmOSWmPG/view
